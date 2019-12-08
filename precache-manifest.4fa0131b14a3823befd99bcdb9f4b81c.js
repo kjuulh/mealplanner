3b21e14c3f2fc678ce3416a8560e5cd375b1d2e6
hermansen <hermansendev@gmail.com>
2019-12-08 13:37:15 +0100

    Updates

---
+++ precache-manifest.4fa0131b14a3823befd99bcdb9f4b81c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae67eb6e273701ddca97152197f93fb",
+    "url": "/mealplanner/index.html"
+  },
+  {
+    "revision": "46031de3ff03e56f936f",
+    "url": "/mealplanner/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0d6eca3dc69d0549c29f",
+    "url": "/mealplanner/static/js/2.7eaf40b1.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/mealplanner/static/js/2.7eaf40b1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46031de3ff03e56f936f",
+    "url": "/mealplanner/static/js/main.055621ea.chunk.js"
+  },
+  {
+    "revision": "2f79cb944d42d7eb2b07",
+    "url": "/mealplanner/static/js/runtime-main.b0e2048f.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Mealplanner/precache-manifest.6c9b840c73dc9ada8838fc2dcf98e049.js"
+  "/mealplanner/precache-manifest.4fa0131b14a3823befd99bcdb9f4b81c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Mealplanner/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mealplanner/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
