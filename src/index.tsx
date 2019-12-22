5fa7982f9a19d0af5a89a31c9d40e112b9683572
kjuulh <hermansendev@gmail.com>
2019-12-22 19:58:42 +0100

    added logrocket

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import LogRocket from 'logrocket';
+LogRocket.init('pqcelz/mealplanner');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
